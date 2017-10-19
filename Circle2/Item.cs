88e934847e88d273cdfe5e94d765624b703b164f
Arick1995 <arick1995@gmail.com>
2017-10-19 19:14:33 +0800

    Add files via upload

---
+++ Circle2/Item.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Item 的摘要描述
+/// </summary>
+public class Item
+{
+    public Item()
+    {
+        //
+        // TODO: 在這裡新增建構函式邏輯
+        //
+    }
+    public string Name
+    {
+        get;
+        set;
+    }
+
+    public string ID
+    {
+        get;
+        set;
+    }
+    public string GetFullName()
+    {
+        return ID + "_" + Name;
+    }
+}
---
+++ Circle2/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Product 的摘要描述
+/// </summary>
+public class Product:Item
+{
+    public Product()
+    {
+        //
+        // TODO: 在這裡新增建構函式邏輯
+        //
+    }
+    public double price
+    {
+        get;
+        set;
+    }
+
+    public double Total_Sales(int volmn)
+    {
+        
+        return volmn * price;
+    }
+
+}
